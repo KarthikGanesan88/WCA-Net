5f7b0f929ff76c3394ceb65cc918fb17de38a58e
Karthik Ganesan <karthik.ganesan@mail.utoronto.ca>
2022-07-20 12:00:09 -0400

    changed preactresnet18 model

--- models/preactresnet.py
+++ models/preactresnet.py
@@ -1,12 +1,10 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as f
-from torch.nn import init
+import torch.nn.functional as F
 
 from .common import VanillaBase, WCANet_Base
 from torch.distributions.normal import Normal
 from torch.distributions.multivariate_normal import MultivariateNormal
-
 
 class PreActBlock(nn.Module):
     """Pre-activation version of the BasicBlock."""
@@ -20,13 +18,15 @@ class PreActBlock(nn.Module):
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
 
         if stride != 1 or in_planes != self.expansion * planes:
-            self.shortcut = nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False)
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False)
+            )
 
     def forward(self, x):
-        out = f.relu(self.bn1(x))
-        shortcut = self.shortcut(x) if hasattr(self, 'shortcut') else x
+        out = F.relu(self.bn1(x))
+        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
         out = self.conv1(out)
-        out = self.conv2(f.relu(self.bn2(out)))
+        out = self.conv2(F.relu(self.bn2(out)))
         out += shortcut
         return out
 
@@ -35,28 +35,13 @@ class PreActResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(PreActResNet, self).__init__()
         self.in_planes = 64
+
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.bn = nn.BatchNorm2d(512 * block.expansion)
         # self.linear = nn.Linear(512 * block.expansion, num_classes)
-        # self.init_params()
-
-    # def init_params(self):
-    #     for m in self.modules():
-    #         if isinstance(m, nn.Conv2d):
-    #             init.kaiming_normal_(m.weight, mode='fan_out')
-    #             if m.bias is not None:
-    #                 init.constant_(m.bias, 0)
-    #         elif isinstance(m, nn.BatchNorm2d):
-    #             init.constant_(m.weight, 1)
-    #             init.constant_(m.bias, 0)
-    #         elif isinstance(m, nn.Linear):
-    #             init.normal_(m.weight, std=0.001)
-    #             if m.bias is not None:
-    #                 init.constant_(m.bias, 0)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -64,22 +49,23 @@ class PreActResNet(nn.Module):
         for stride in strides:
             layers.append(block(self.in_planes, planes, stride))
             self.in_planes = planes * block.expansion
-        return nn.ModuleList(layers)
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = f.relu(self.bn(x))
-        x = f.avg_pool2d(x, 4)
-        x = x.view(x.size(0), -1)
-        # x = self.linear(x)
-        return x
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        # out = self.linear(out)
+        return out
 
-def PreActResNet18(num_classes=10):
-    return PreActResNet(PreActBlock, [2, 2, 2, 2], num_classes=num_classes)
+
+def PreActResNet18():
+    return PreActResNet(PreActBlock, [2, 2, 2, 2])
+
 
 ########################################################
 #    PreActResNet18
@@ -104,7 +90,7 @@ class VanillaPreActResNet18(VanillaBase):
 
     def forward(self, x):
         x = self.gen(x)
-        x = f.relu(self.fc1(x))
+        x = F.relu(self.fc1(x))
         x = self.proto(x)
         return x
 
@@ -121,9 +107,9 @@ class PreActResNet18_StochasticBaseDiagonal(nn.Module):
 
     def forward(self, x):
         x = self.gen(x)
-        x = f.relu(self.fc1(x))
+        x = F.relu(self.fc1(x))
         if not self.disable_noise:
-            dist = Normal(0., f.softplus(self.sigma))
+            dist = Normal(0., F.softplus(self.sigma))
             x_sample = dist.rsample()
             x = x + x_sample
         return x
@@ -146,12 +132,13 @@ class PreActResNet18_StochasticBaseMultivariate(nn.Module):
 
     def forward(self, x):
         x = self.gen(x)
-        x = f.relu(self.fc1(x))
+        x = F.relu(self.fc1(x))
         if not self.disable_noise:
             dist = MultivariateNormal(self.mu, scale_tril=self.L, validate_args=False)
             x_sample = dist.rsample()
             x = x + x_sample
         return x
+
 
 class WCANet_PreActResNet18(WCANet_Base):
     def __init__(self, D, C, variance_type, disable_noise=False):
@@ -170,5 +157,3 @@ class WCANet_PreActResNet18(WCANet_Base):
         x = self.base(x)
         x = self.proto(x)
         return x
-
-
